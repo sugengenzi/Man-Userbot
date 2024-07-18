d8d24a3576a892de12c121b81bbb94a494ff60d9
sugengenzi <144963928+sugengenzi@users.noreply.github.com>
2024-07-18 23:32:34 +0700

    Create Velar.js

---
+++ Velar.js
@@ -0,0 +1,9 @@
+cconst velar = require('velar')
+
+var dir =  'src/assets/',
+    vendor = {
+        "jquery": "3.3.1",
+        "OwlCarousel2": "2.3.4",
+        "twitter-bootstrap": "4.3.1",
+        "vue": "2.6.8"
+    }
